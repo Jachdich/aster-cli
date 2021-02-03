cabd634841c031f1415a441c41d87a86d5f762ad
Noah <nobrotojak@hotmail.com>
2021-02-03 16:57:26 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use openssl::ssl::{SslMethod, SslConnector, SslVerifyMode};
 use std::net::TcpStream;
 use std::io::{Read, Write};
 use std::str::from_utf8;
+use std::io;
 
 fn main() {
     match TcpStream::connect("127.0.0.1:42069") {
@@ -15,18 +16,27 @@ fn main() {
             
             let mut sslstream = connector.connect("127.0.0.1", stream).unwrap();
 
-            let msg = b"hello, world";
-            sslstream.write(msg).unwrap(); //todo handle error
-            let mut data = [0 as u8; 12];
-            match sslstream.read_exact(&mut data) {
-                Ok(_) => {
-                    let text = from_utf8(&data).unwrap();
-                    println!("{}", text);
-                },
-                Err(e) => {
-                    println!("Failed to recv data: {}", e);
+            loop{
+                let mut input = String::new();
+                let mut stdin = io::stdin(); // We get `Stdin` here.
+                stdin.read_line(&mut input);
+
+                let msg = input.as_bytes();
+
+                sslstream.write(msg).unwrap(); //todo handle error
+                let mut data = vec![0 as u8; msg.len()];
+                match sslstream.read_exact(&mut data) {
+                    Ok(_) => {
+                        let text = from_utf8(&data).unwrap();
+                        println!("{}", text);
+                    },
+                    Err(e) => {
+                        println!("Failed to recv data: {}", e);
+                    }
                 }
             }
+
+            
         },
         Err(e) => {
             println!("Failed to connect: {}", e);
